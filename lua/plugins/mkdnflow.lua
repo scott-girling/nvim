884bfcd7dd6abbb4e93957bb883ccf63a12f5b34
Scott Girling <scottgirling@gmail.com>
2025-02-25 17:07:47 +1000

    refactor: change mkdnflow config

--- lua/plugins/mkdnflow.lua
+++ lua/plugins/mkdnflow.lua
@@ -1,5 +1,23 @@
 local okay, mkdnflow = pcall(require, 'mkdnflow')
 
 if okay then
-	mkdnflow.setup()
+	mkdnflow.setup({
+		links = {
+			transform_explicit = function(text)
+				text = text:gsub(" ", "-")
+				text = text:lower()
+				text = 'notes/' .. text
+				return(text)
+			end
+		},
+		new_file_template = {
+			use_template = true,
+			template = [[
+---
+tags:
+---
+# {title}
+]]
+		}
+	})
 end
