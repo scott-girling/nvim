d2bd7b9bcc45df8d66736df5944f9164e9d9795c
Scott Girling <scottgirling@gmail.com>
2025-06-25 10:47:49 +1000

    refactor: hide listchars when viewing a diff

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -3,3 +3,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank { higroup="Visual", timeout=150 }
 	end
 })
+
+-- Set listchars to be hidden when using Neovim as a git difftool
+vim.api.nvim_create_autocmd({"DiffUpdated", "BufRead"}, {
+	callback = function()
+		if vim.o.diff then
+			vim.o.list = false
+		else
+			vim.o.list = true
+		end
+	end
+})
