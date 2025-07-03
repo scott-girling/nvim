aa463dd1f5f0b05c91ec9b5c72d1e59a75a104e0
Scott Girling <scottgirling@gmail.com>
2025-07-03 13:31:34 +1000

    fix: toggle neotree on keymap press

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -3,7 +3,7 @@ local okay, _ = pcall(require, 'neo-tree')
 if okay then
 	local s = vim.keymap.set
 
-	s("n", "<leader>e", ":Neotree filesystem reveal<cr>")
+	s("n", "<leader>e", ":Neotree toggle reveal<cr>")
 	s("n", "-", "<cmd>Neotree position=current reveal<cr>")
 	s("n", "<leader><leader>", ":Neotree buffers reveal<cr>")
 end
